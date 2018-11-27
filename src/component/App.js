9247b9cc0f3fabf9d5bdf8cd1329c396cb1ecae7
kim <jina021176@gmail.com>
2018-11-27 22:59:31 +0900

    a

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Home from './home/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+          <Home/>
         </header>
       </div>
     );
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import "./Home.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <section id="home" className="home">
+                <div className="home__header">
+                    <div className="home-header__row">
+                        <div className="home-header__title">
+                            Hello, I'm 
+                            <span className="home-header__title-highlight"> JA ♥ JH</span>. <br/>
+                            I'm a full-stack web developer. <br/>
+                        </div>
+                        <button className="home-header__btn--over">View my work ->
+                        <i class="icon-angle-right"></i>
+                        </button>
+                    </div>
+                </div>
+                <div class="home__navi">
+                    <div class="home-navi__menu--top">
+                        <div class="navi-menu__title"></div>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
 
 export default Home;
+
