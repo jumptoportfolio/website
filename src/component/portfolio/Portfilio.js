47de8e40eee8bdf84339e8873f5c2ef31d43c231
parkoon <par_koon@naver.com>
2018-11-27 14:30:41 +0900

    init project

---
+++ src/component/portfolio/Portfilio.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Portfilio extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Portfilio;
