56968cc63126db184d435cc81205481354983aca
parkoon <par_koon@naver.com>
2018-11-27 15:34:58 +0900

    reset css 적용 및 다른 작업중

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import Header from '../header/Header'
+
+const TITLE = 'ABOUT';
+const SUB_TITLE = 'SUBTITLE';
+
 class About extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="about">
+                <Header
+                    title={TITLE}
+                    subtitle={SUB_TITLE}
+                />
             </div>
         );
     }
