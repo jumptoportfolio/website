3bf15293da03edff0546c43222b619a01b49c1c5
DESKTOP-GT50FQ3\ProDesk <jina021176@gmail.com>
2018-11-29 17:26:59 +0900

    Updates

---
+++ precache-manifest.de1546cd8bf084820760fb3508b0413a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "62e8050e9399ae37c7b1",
+    "url": "/website/static/js/main.62e8050e.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/website/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "62e8050e9399ae37c7b1",
+    "url": "/website/static/css/main.32976e80.chunk.css"
+  },
+  {
+    "revision": "c1975005de014c7eb9b2ccc894d67b12",
+    "url": "/website/index.html"
+  }
+];
