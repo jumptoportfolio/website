cfbfb732a9cd66fabd36862623548e456c498b52
kim <jina021176@gmail.com>
2018-12-02 15:28:05 +0900

    icon

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import "./Home.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Icon } from 'react-fa';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false
+        }
+    }
+    hoverOn= () => {
+        this.setState({ hover: true });
+    };
+    hoverOff= () => {
+        this.setState({ hover: false });    
+    };
     render() {
         return (
             <section id="home" className="home">
+            <div id="container"></div>
                 <div className="home__header">
                     <div className="home-header__row">
                         <div className="home-header__title">
@@ -13,8 +26,12 @@ class Home extends Component {
                             <span className="home-header__title-highlight"> JA ♥ JH</span>. <br/>
                             I'm a full-stack web developer. <br/>
                         </div>
-                        <button className="home-header__btn--over">View my work ->
-                        <i class="icon-angle-right"></i>
+                        <button className="home-header__btn--over" 
+                        onMouseOver={this.hoverOn} 
+                        onMouseLeave={this.hoverOff} >
+                        View my work  &nbsp;
+                        {/* <Icon name="arrow-right"/> */}
+                        <i className={ this.state.hover ? "fa fa-arrow-down" : "fa fa-arrow-right"} > </i>
                         </button>
                     </div>
                 </div>
@@ -29,4 +46,3 @@ class Home extends Component {
 }
 
 export default Home;
-
