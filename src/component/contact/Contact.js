58c8548138fe37510ebfd47f84ea642e09aa5480
parkoon <par_koon@naver.com>
2018-12-06 18:00:43 +0900

    contact us! form effect 적용
    button 추가해야함!

--- src/component/contact/Contact.js
+++ src/component/contact/Contact.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import './Contact.css';
+
+import Header from '../header/Header'
+
+const TITLE = 'CONTACT'
+const SUB_TITLE = 'Have a question or wanna work together?'
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                Contact!
+            <div className="contact">
+                <Header
+                    title={TITLE}
+                    subtitle={SUB_TITLE}
+                    titleColor={'#fff'}
+                    subtitleColor={'#04C2C9'}
+                />
+
+                <div className="contact-form__container">
+                    <form>
+                        <div className="contact-form__left">
+                            <div className="contact-form__box">
+                                <input type="text" required></input>
+                                <label>Your name</label>
+                            </div>
+                            <div className="contact-form__box">
+                                <input type="text" required></input>
+                                <label>Your email</label>
+                            </div>
+
+                        </div>
+                        <div className="contact-form__right">
+                            <div className="contact-form__box">
+                                <textarea type="text" required></textarea>
+                                <label>Your message</label>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
