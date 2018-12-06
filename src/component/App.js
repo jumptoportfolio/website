3baaa2a3e43d62c274d8c9d0f83f6f71a2a08790
parkoon <par_koon@naver.com>
2018-12-06 14:01:24 +0900

    폰트적용

--- src/component/App.js
+++ src/component/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import About from './about/About'
 import Home from './home/Home';
 import Portfolio from './portfolio/Portfolio'
+import Contact from './contact/Contact'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <Home/>
           <About />
           <Portfolio />
+          <Contact />
 
       </div>
     );
