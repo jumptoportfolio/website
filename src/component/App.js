f16f50950504e40494faa738b444b6c2a5fa74de
parkoon <par_koon@naver.com>
2018-12-07 17:30:58 +0900

    ## Home 수정 ### 수정내용 1. bounce arrow 2. logo with animation 3. hamberger menu
    ### 보완필요
    1. 스크롤 했을 때 네비바 처리

--- src/component/App.js
+++ src/component/App.js
@@ -7,10 +7,45 @@ import Contact from './contact/Contact';
 import Parallax from './parallax/Parallax';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isTop: true,
+      isHambergerCliked: false,
+    }
+
+    this._hambergerHandler = this._hambergerHandler.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+        this.setState({
+          isTop
+        })
+      }
+    })
+  }
+
+  _hambergerHandler() {
+    this.setState({
+      isHambergerCliked: !this.state.isHambergerCliked
+    })
+  }
+
+
   render() {
+
+    const { isTop, isHambergerCliked } = this.state
+    
     return (
       <div className="App">
-          <Home/>
+          <Home 
+            isTop={isTop}
+            isHambergerCliked={isHambergerCliked}
+            hambergerHandler={this._hambergerHandler}
+            />
           <About />
           <Parallax />
           <Portfolio />
--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,49 +1,57 @@
-import React, { Component } from 'react';
-import "./Home.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Home.css';
 import 'react-fa';
 
+const Home = ({ isTop, hambergerHandler, isHambergerCliked }) => {
+    console.log('HOME IS RENDERED', isTop)
 
-class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hover: false
-        }
-    }
-    hoverOn= () => {
-        this.setState({ hover: true });
-    };
-    hoverOff= () => {
-        this.setState({ hover: false });    
-    };
-    render() {
-        return (
-            <section id="home" className="home">
-            <div id="container"></div>
-                <div className="home__header">
-                    <div className="home-header__row">
-                        <div className="home-header__title">
-                            Hello, Welcome to 
-                            <span className="home-header__title-highlight"> MY HOMEPAGE</span>. <br/>
-                            I'm a full-stack web developer. <br/>
-                        </div>
-                        <button className="home-header__btn--over" 
-                        onMouseOver={this.hoverOn} 
-                        onMouseLeave={this.hoverOff} >
-                        View my work  &nbsp;
-                        {/* <Icon name="arrow-right"/> */}
-                        <i className={ this.state.hover ? "fa fa-arrow-down" : "fa fa-arrow-right"} > </i>
-                        </button>
+    return (
+        <div className="home">
+            <header className="home__header">
+                <div className="home-header__logo">
+                    <i className="fab fa-apple"></i>
+                    <span className="subtitle">진아야공부하자</span>
+                </div>
+                <div className="home-header__nav">
+
+                    <div className={`nav__menu ${isHambergerCliked ? 'active': ''}`}>
+                        <span>Home</span>
+                        <span>About</span>
+                        <span>Portfolio</span>
+                        <span>Study</span>
+                        <span>Contact</span>
+                    </div>
+                    <div 
+                        className={`nav__hamberger ${isHambergerCliked ? 'active': ''}`}
+                        onClick={hambergerHandler}
+                    >
+                        <div className="nav__hamberger--line"></div>
+                        <div className="nav__hamberger--line"></div>
+                        <div className="nav__hamberger--line"></div>
                     </div>
                 </div>
-                <div class="home__navi">
-                    <div class="home-navi__menu--top">
-                        <div class="navi-menu__title"></div>
-                    </div>
+            </header>
+            <div className="home__details">
+                <div className="home-details__text">
+                    Hello, I'm <span className="home-details__text--highlight">Kim Jin A</span>
                 </div>
-            </section>
-        );
-    }
-}
+                <div className="home-details__text">
+                    I'm <span className="home-details__text--underline">full-stack</span> web developer
+                </div>
+                {/* <div className="home-details__button">
+                    View my works
+                </div> */}
+                <div className="home-details__arrow bounce">
+                    <i className="fa fa-arrow-down"></i>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+Home.propTypes = {
+    
+};
 
 export default Home;
