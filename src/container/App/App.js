016e3d1152fe290ebdf01a4d32d9100727751b00
parkoon <par_koon@naver.com>
2018-12-11 17:53:58 +0900

    init

---
+++ src/container/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+
+  }
+
+  render() {
+    return(
+      <div>
+        버전2
+      </div>
+    )
+  }
+}
+
+export default App;
